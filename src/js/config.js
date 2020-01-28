65e6bd07d5286a55906c10a43c5ae24aa2c773a8
Sergey Chirva <19sem92@gmail.com>
2020-01-28 09:47:56 +0300

    TASK-3-replace mock data with real data coming from the API

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,8 @@
-const API_KEY = '8a27658d7a5b02060836af27a9c31d3a'
+const API_KEY = "8a27658d7a5b02060836af27a9c31d3a";
 
 export default {
-    localMoviesUrl: '../../mock-data/movies.json',
-    imageSrc: 'http://image.tmdb.org/t/p/w185',
-}
+    localMoviesUrl: "../../mock-data/movies.json",
+    imageSrc: "http://image.tmdb.org/t/p/w185",
+    playNow: page =>
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=${page}`
+};
